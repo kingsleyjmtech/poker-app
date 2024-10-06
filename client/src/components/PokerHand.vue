b1b2713c1a10e8124da5f1f5e37f4623407cd7b0
Kingsley J Moseamedi <kingsleyjmtech@gmail.com>
2024-10-06 16:08:14 +0200

    Make UI responsive and update button

--- client/src/components/PokerHand.vue
+++ client/src/components/PokerHand.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white">
+  <div class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white px-4">
     <header class="text-center mb-8">
-      <h1 class="text-5xl font-bold mb-4">Poker Game</h1>
-      <p class="text-xl">Test your luck and skill with a hand of poker!</p>
+      <h1 class="text-4xl sm:text-5xl font-bold mb-4">Poker Game</h1>
+      <p class="text-lg sm:text-xl">Test your luck and skill with a hand of poker!</p>
     </header>
 
-    <div class="bg-white text-gray-900 rounded-lg shadow-lg p-8 w-full max-w-lg text-center my-1">
-      <h2 class="text-3xl font-semibold mb-6">
+    <div class="bg-white text-gray-900 rounded-lg shadow-lg p-6 sm:p-8 w-full max-w-md sm:max-w-lg text-center my-1">
+      <h2 class="text-2xl sm:text-3xl font-semibold mb-6">
         Your Hand:
         <span v-for="(card, index) in hand" :key="index" :class="getCardColorClass(card)" class="mx-1">
           {{ card }}
         </span>
       </h2>
-      <h3 class="text-2xl text-green-500 font-bold mb-4">You have: {{ evaluation }}</h3>
+      <h3 class="text-xl sm:text-2xl text-green-500 font-bold mb-4">You have: {{ evaluation }}</h3>
 
-      <div v-if="errorMessage" class="error text-red-600 text-lg">
+      <div v-if="errorMessage" class="error text-red-600 text-sm sm:text-lg">
         <p>Error: {{ errorMessage }} ({{ statusCode }})</p>
       </div>
     </div>
 
     <ShuffleButton />
 
-    <footer class="mt-12">
-      <p class="text-lg">&copy; {{ currentYear }} Poker World. All rights reserved.</p>
+    <footer class="mt-8 sm:mt-12">
+      <p class="text-sm sm:text-lg">&copy; {{ currentYear }} Poker World. All rights reserved.</p>
     </footer>
   </div>
 </template>
--- client/src/components/ShuffleButton.vue
+++ client/src/components/ShuffleButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
       @click="shuffleHand"
-      class="mt-10 px-8 py-4 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-bold text-xl rounded-full shadow-lg transition duration-300 ease-in-out"
+      class="mt-6 sm:mt-10 px-6 sm:px-8 py-3 sm:py-4 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-bold text-lg sm:text-xl rounded-full shadow-lg transition duration-300 ease-in-out"
   >
     {{ evaluation ? 'Shuffle and Deal Again' : 'Shuffle and Deal' }}
   </button>
